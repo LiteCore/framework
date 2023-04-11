0f3bb2c6bf6f185bdf4cf67fe7468f571acd49fd
T. Almroth <info@tim-international.net>
2023-04-11 17:05:21 +0200

    + Added robots.txt

--- public_html/frontend/pages/robots.txt.inc.php
+++ public_html/frontend/pages/robots.txt.inc.php
@@ -1,10 +1,9 @@
 <?php
-	
-	header('Content-Type: text/plain;charset='. language::$selected['charset']);
+	header('Content-Type: text/plain;charset='. mb_http_output());
 ?>
 User-agent: *
 Allow: /
 Disallow: */cache/*
-Sitemap: <?php echo document::ilink('feeds/sitemap.xml'); ?>
+Sitemap: <?php echo document::ilink('sitemap.xml'); ?>
 <?php
 	exit;
