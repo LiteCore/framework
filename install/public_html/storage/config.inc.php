7a0ea4c0a2ef30b0f9ca6ef53470d9293f57b292
T. Almroth <info@tim-international.net>
2023-12-11 01:16:17 +0100

    * Config adjustments

--- install/public_html/storage/config.inc.php
+++ install/public_html/storage/config.inc.php
@@ -1,52 +1,75 @@
 <?php
 
-  define('BACKEND_ALIAS', 'admin');
+	define('BACKEND_ALIAS', 'admin');
 
-######################################################################
-## Files and Directory  ##############################################
-######################################################################
+	######################################################################
+	## Files and Directory  ##############################################
+	######################################################################
 
-// File System
-  define('DOCUMENT_ROOT',    str_replace('\\', '/', rtrim(realpath(!empty($_SERVER['DOCUMENT_ROOT']) ? $_SERVER['DOCUMENT_ROOT'] : __DIR__.'/..'), '/')));
+	// File System
+	define('DOCUMENT_ROOT',    str_replace('\\', '/', rtrim(realpath(!empty($_SERVER['DOCUMENT_ROOT']) ? $_SERVER['DOCUMENT_ROOT'] : __DIR__.'/..'), '/')));
 
-  define('FS_DIR_APP',       str_replace('\\', '/', rtrim(realpath(__DIR__.'/..'), '/')) . '/');
-  define('FS_DIR_ASSETS',    FS_DIR_APP . 'assets/');
-  define('FS_DIR_STORAGE',   FS_DIR_APP . 'storage/');
-  define('FS_DIR_VENDOR',    FS_DIR_APP . 'vendor/');
+	define('FS_DIR_APP',       str_replace('\\', '/', rtrim(realpath(__DIR__.'/..'), '/')) . '/');
+	define('FS_DIR_ASSETS',    FS_DIR_APP . 'assets/');
+	define('FS_DIR_STORAGE',   FS_DIR_APP . 'storage/');
+	define('FS_DIR_VENDOR',    FS_DIR_APP . 'vendor/');
 
-// Web System
-  define('WS_DIR_APP',       preg_replace('#^'. preg_quote(DOCUMENT_ROOT, '#') .'#', '', FS_DIR_APP));
-  define('WS_DIR_ADMIN',     WS_DIR_APP . BACKEND_ALIAS . '/');
-  define('WS_DIR_ASSETS',    WS_DIR_APP . 'assets/');
-  define('WS_DIR_STORAGE',   WS_DIR_APP . 'storage/');
-  define('WS_DIR_VENDOR',    WS_DIR_APP . 'vendor/');
+	// Web System
+	define('WS_DIR_APP',       preg_replace('#^'. preg_quote(DOCUMENT_ROOT, '#') .'#', '', FS_DIR_APP));
+	define('WS_DIR_ADMIN',     WS_DIR_APP . BACKEND_ALIAS . '/');
+	define('WS_DIR_ASSETS',    WS_DIR_APP . 'assets/');
+	define('WS_DIR_STORAGE',   WS_DIR_APP . 'storage/');
+	define('WS_DIR_VENDOR',    WS_DIR_APP . 'vendor/');
 
-######################################################################
-## Database ##########################################################
-######################################################################
+	######################################################################
+	## Database ##########################################################
+	######################################################################
 
-// Database
-  define('DB_TYPE', 'mysql');
-  define('DB_SERVER', '127.0.0.1');
-  define('DB_USERNAME', 'root');
-  define('DB_PASSWORD', '');
-  define('DB_DATABASE', 'litebase');
-  define('DB_TABLE_PREFIX', 'lb_');
-  define('DB_CONNECTION_CHARSET', 'utf8mb4');
+	// Database
+	define('DB_TYPE', 'mysql');
+	define('DB_SERVER', '127.0.0.1');
+	define('DB_USERNAME', '');
+	define('DB_PASSWORD', '');
+	define('DB_DATABASE', '');
+	define('DB_TABLE_PREFIX', 'lc_');
+	define('DB_CONNECTION_CHARSET', 'utf8mb4');
 
-######################################################################
-## Application #######################################################
-######################################################################
+	######################################################################
+	## System ############################################################
+	######################################################################
 
-// Errors
-  error_reporting(version_compare(PHP_VERSION, '5.4.0', '>=') ? E_ALL & ~E_STRICT : E_ALL);
-  ini_set('ignore_repeated_errors', 'On');
-  ini_set('log_errors', 'On');
-  ini_set('error_log', FS_DIR_STORAGE . 'logs/errors.log');
-  ini_set('display_startup_errors', 'Off');
-  ini_set('display_errors', 'Off');
-  if (in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1'])) {
-    error_reporting(E_ALL);
-    ini_set('display_startup_errors', 'On');
-    ini_set('display_errors', 'On');
-  }
+	// Set Character Encoding
+	mb_internal_encoding('UTF-8');
+	mb_regex_encoding('UTF-8');
+	mb_http_output('UTF-8');
+
+	// Floating Point Precision
+	ini_set('serialize_precision', 6);
+
+	// Script timeout
+	set_time_limit(60);
+
+	// Sessions
+	ini_set('session.name', 'LCSESSID');
+	ini_set('session.use_cookies', 1);
+	ini_set('session.use_only_cookies', 1);
+	ini_set('session.use_strict_mode', 1);
+	ini_set('session.use_trans_sid', 0);
+	ini_set('session.cookie_httponly', 1);
+	ini_set('session.cookie_lifetime', 0);
+	ini_set('session.cookie_path', WS_DIR_APP);
+	ini_set('session.cookie_samesite', 'Lax');
+	ini_set('session.gc_maxlifetime', 1440);
+
+	// Errors
+	error_reporting(version_compare(PHP_VERSION, '5.4.0', '>=') ? E_ALL & ~E_STRICT : E_ALL);
+	ini_set('ignore_repeated_errors', 'On');
+	ini_set('log_errors', 'On');
+	ini_set('error_log', FS_DIR_STORAGE . 'logs/errors.log');
+	ini_set('display_startup_errors', 'Off');
+	ini_set('display_errors', 'Off');
+	if (in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1'])) {
+		error_reporting(E_ALL);
+		ini_set('display_startup_errors', 'On');
+		ini_set('display_errors', 'On');
+	}
