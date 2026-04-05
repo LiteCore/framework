2e1f6fe7321691c765254ae6c4bd010d637af52d
Wachhund Skytower <Wachhund@users.noreply.github.com>
2026-04-06 00:50:48 +0200

    * Rewrite carousel: drop Bootstrap dependency, add touch/swipe support

--- public_html/assets/litecore/js/components/carousel.js
+++ public_html/assets/litecore/js/components/carousel.js
@@ -1,162 +1,155 @@
 /*
- * Bootstrap: carousel.js v3.4.1
- * https://getbootstrap.com/docs/3.4/javascript/#carousel
- *
- * Copyright 2011-2019 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ * Simplified Carousel with Swipe Support
+ * Lightweight carousel with essential features
  */
 
 waitFor('jQuery', ($) => {
-	'use strict'
+	'use strict';
 
 	class Carousel {
 		constructor(element, options) {
 			this.$element = $(element);
 			this.$indicators = this.$element.find('.carousel-indicators');
-			this.options = options;
-			this.paused = null;
-			this.sliding = null;
-			this.interval = null;
-			this.$active = null;
-			this.$items = null;
+			this.options = $.extend({}, Carousel.DEFAULTS, options);
 
+			this.paused = false;
+			this.sliding = false;
+			this.interval = null;
+			this.touchStartX = 0;
+
+			this.init();
+		}
+
+		init() {
+			this.$items = this.$element.find('.item');
+			this.$active = this.$element.find('.item.active');
+
+			// Event listeners
+			this.setupEvents();
+
+			// Start autoplay
+			if (this.options.interval) {
+				this.cycle();
+			}
+		}
+
+		setupEvents() {
+			// Keyboard navigation
 			if (this.options.keyboard) {
-				this.$element.on('keydown.carousel', (e) => this.keydown(e));
+				this.$element.on('keydown.carousel', (e) => {
+					if (/input|textarea/i.test(e.target.tagName)) return;
+					if (e.which === 37) { this.prev(); e.preventDefault(); }
+					if (e.which === 39) { this.next(); e.preventDefault(); }
+				});
 			}
 
-			if (this.options.pause === 'hover' && !('ontouchstart' in document.documentElement)) {
+			// Mouse hover
+			if (this.options.pause === 'hover') {
 				this.$element
 					.on('mouseenter.carousel', () => this.pause())
 					.on('mouseleave.carousel', () => this.cycle());
 			}
-		}
 
-		static get DEFAULTS() {
-			return {
-				interval: 5000,
-				pause: 'hover',
-				wrap: true,
-				keyboard: true
-			};
-		}
+			// Touch/swipe
+			if (this.options.touch) {
+				this.$element[0].addEventListener('touchstart', (e) => {
+					if (e.touches.length === 1) {
+						this.touchStartX = e.touches[0].clientX;
+					}
+				}, { passive: true });
 
-		keydown(e) {
-			if (/input|textarea/i.test(e.target.tagName)) return;
-			switch (e.which) {
-				case 37: this.prev(); break;
-				case 39: this.next(); break;
-				default: return;
+				this.$element[0].addEventListener('touchend', (e) => {
+					if (e.changedTouches.length === 1) {
+						const deltaX = e.changedTouches[0].clientX - this.touchStartX;
+						if (Math.abs(deltaX) > 50) {
+							deltaX > 0 ? this.prev() : this.next();
+						}
+					}
+				}, { passive: true });
 			}
-			e.preventDefault();
 		}
 
-		cycle(e) {
-			if (!e) this.paused = false;
-
+		cycle() {
+			this.paused = false;
 			if (this.interval) clearInterval(this.interval);
-
-			if (this.options.interval && !this.paused) {
+			if (this.options.interval) {
 				this.interval = setInterval(() => this.next(), this.options.interval);
 			}
-
 			return this;
 		}
 
-		getItemIndex(item) {
-			this.$items = item.parent().children('.item');
-			return this.$items.index(item || this.$active);
-		}
-
-		getItemForDirection(direction, active) {
-			const activeIndex = this.getItemIndex(active);
-			const willWrap = (direction === 'prev' && activeIndex === 0) ||
-							 (direction === 'next' && activeIndex === (this.$items.length - 1));
-			if (willWrap && !this.options.wrap) return active;
-
-			const delta = direction === 'prev' ? -1 : 1;
-			const itemIndex = (activeIndex + delta) % this.$items.length;
-			return this.$items.eq(itemIndex);
-		}
-
-		to(pos) {
-			const activeIndex = this.getItemIndex(this.$active = this.$element.find('.item.active'));
-
-			if (pos > (this.$items.length - 1) || pos < 0) return;
-
-			if (this.sliding) {
-				return this.$element.one('slid.carousel', () => this.to(pos));
+		pause() {
+			this.paused = true;
+			if (this.interval) {
+				clearInterval(this.interval);
+				this.interval = null;
 			}
-			if (activeIndex === pos) return this.pause().cycle();
-
-			return this.slide(pos > activeIndex ? 'next' : 'prev', this.$items.eq(pos));
-		}
-
-		pause(e) {
-			if (!e) this.paused = true;
-
-			if (this.$element.find('.next, .prev').length && $.support.transition) {
-				this.$element.trigger($.support.transition.end);
-				this.cycle(true);
-			}
-
-			clearInterval(this.interval);
-			this.interval = null;
-
 			return this;
 		}
 
 		next() {
-			if (this.sliding) return;
+			if (this.sliding) return this;
 			return this.slide('next');
 		}
 
 		prev() {
-			if (this.sliding) return;
+			if (this.sliding) return this;
 			return this.slide('prev');
+		}
+
+		to(index) {
+			const activeIndex = this.$items.index(this.$active);
+			if (index < 0 || index >= this.$items.length || index === activeIndex) return this;
+			if (this.sliding) return this.$element.one('slid.carousel', () => this.to(index));
+
+			const direction = index > activeIndex ? 'next' : 'prev';
+			return this.slide(direction, this.$items.eq(index));
 		}
 
 		slide(type, next) {
 			const $active = this.$element.find('.item.active');
-			const $next = next || this.getItemForDirection(type, $active);
-			const isCycling = this.interval;
+			const $next = next || this.getNext(type, $active);
 			const direction = type === 'next' ? 'left' : 'right';
 
-			if ($next.hasClass('active')) return (this.sliding = false);
+			if ($next.hasClass('active') || this.sliding) return this;
 
-			const relatedTarget = $next[0];
+			// Trigger slide event
 			const slideEvent = $.Event('slide.carousel', {
-				relatedTarget,
-				direction
+				relatedTarget: $next[0],
+				direction: direction
 			});
 			this.$element.trigger(slideEvent);
-			if (slideEvent.isDefaultPrevented()) return;
+			if (slideEvent.isDefaultPrevented()) return this;
 
 			this.sliding = true;
+			const wasCycling = this.interval;
+			wasCycling && this.pause();
 
-			if (isCycling) this.pause();
-
+			// Update indicators
 			if (this.$indicators.length) {
 				this.$indicators.find('.active').removeClass('active');
-				const $nextIndicator = $(this.$indicators.children()[this.getItemIndex($next)]);
-				if ($nextIndicator) $nextIndicator.addClass('active');
+				const nextIndex = this.$items.index($next);
+				$(this.$indicators.children()[nextIndex]).addClass('active');
 			}
 
-			const slidEvent = $.Event('slid.carousel', { relatedTarget, direction });
+			// Perform slide
+			const slidEvent = $.Event('slid.carousel', {
+				relatedTarget: $next[0],
+				direction: direction
+			});
+
 			if ($.support.transition && this.$element.hasClass('slide')) {
 				$next.addClass(type);
-				if (typeof $next === 'object' && $next.length) {
-					$next[0].offsetWidth; // force reflow
-				}
+				$next[0].offsetWidth; // Force reflow
 				$active.addClass(direction);
 				$next.addClass(direction);
-				$active
-					.one('bsTransitionEnd', () => {
-						$next.removeClass(`${type} ${direction}`).addClass('active');
-						$active.removeClass(`active ${direction}`);
-						this.sliding = false;
-						setTimeout(() => this.$element.trigger(slidEvent), 0);
-					})
-					.emulateTransitionEnd(600);
+
+				$active.one('bsTransitionEnd', () => {
+					$next.removeClass([type, direction].join(' ')).addClass('active');
+					$active.removeClass(['active', direction].join(' '));
+					this.sliding = false;
+					setTimeout(() => this.$element.trigger(slidEvent), 0);
+				}).emulateTransitionEnd(600);
 			} else {
 				$active.removeClass('active');
 				$next.addClass('active');
@@ -164,65 +157,82 @@ waitFor('jQuery', ($) => {
 				this.$element.trigger(slidEvent);
 			}
 
-			if (isCycling) this.cycle();
-
+			wasCycling && this.cycle();
 			return this;
+		}
+
+		getNext(direction, active) {
+			const activeIndex = this.$items.index(active);
+			const isGoingToWrap = (direction === 'prev' && activeIndex === 0) ||
+								  (direction === 'next' && activeIndex === this.$items.length - 1);
+
+			if (isGoingToWrap && !this.options.wrap) return active;
+
+			const delta = direction === 'prev' ? -1 : 1;
+			const itemIndex = (activeIndex + delta) % this.$items.length;
+			return this.$items.eq(itemIndex);
 		}
 	}
 
-	// CAROUSEL PLUGIN DEFINITION
+	Carousel.DEFAULTS = {
+		interval: 5000,
+		pause: 'hover',
+		wrap: true,
+		keyboard: true,
+		touch: true
+	};
 
+	// Plugin definition
 	function Plugin(option) {
 		return this.each(function () {
 			const $this = $(this);
 			let data = $this.data('carousel');
-			const options = { ...Carousel.DEFAULTS, ...$this.data(), ...(typeof option === 'object' && option) };
-			const action = typeof option === 'string' ? option : options.slide;
+			const options = $.extend({}, Carousel.DEFAULTS, $this.data(), typeof option === 'object' && option);
 
 			if (!data) $this.data('carousel', (data = new Carousel(this, options)));
 			if (typeof option === 'number') data.to(option);
-			else if (action) data[action]();
+			else if (typeof option === 'string') data[option]();
 			else if (options.interval) data.pause().cycle();
 		});
 	}
 
-	const old = $.fn.carousel;
-
 	$.fn.carousel = Plugin;
 	$.fn.carousel.Constructor = Carousel;
 
-	// CAROUSEL NO CONFLICT
-
-	$.fn.carousel.noConflict = function () {
-		$.fn.carousel = old;
-		return this;
-	};
-
-	const clickHandler = (e) => {
-		const $this = $(e.currentTarget);
+	// Data API
+	$(document).on('click.carousel.data-api', '[data-slide]', function (e) {
+		const $this = $(this);
 		const $target = $($this.attr('data-target') || $this.closest('.carousel'));
 		if (!$target.hasClass('carousel')) return;
 
-		const options = { ...$target.data(), ...$this.data() };
+		const options = $.extend({}, $target.data(), $this.data());
 		const slideIndex = $this.attr('data-slide-to');
 
 		if (slideIndex) options.interval = false;
-
 		Plugin.call($target, options);
-
-		if (slideIndex) {
-			$target.data('carousel').to(slideIndex);
-		}
-
+		if (slideIndex) $target.data('carousel').to(slideIndex);
 		e.preventDefault();
-	};
+	});
 
-	$(document).on('click.carousel.data-api', '[data-slide], [data-slide-to]', clickHandler);
+	// Arrow controls
+	$(document).on('click.carousel.data-api', '[data-slide="prev"]', function (e) {
+		const $target = $($(this).attr('data-target') || $(this).closest('.carousel'));
+		if (!$target.hasClass('carousel')) return;
+		Plugin.call($target, 'prev');
+		e.preventDefault();
+	});
+
+	$(document).on('click.carousel.data-api', '[data-slide="next"]', function (e) {
+		const $target = $($(this).attr('data-target') || $(this).closest('.carousel'));
+		if (!$target.hasClass('carousel')) return;
+		Plugin.call($target, 'next');
+		e.preventDefault();
+	});
 
 	$(window).on('load', () => {
 		$('[data-ride="carousel"]').each(function () {
-			const $carousel = $(this);
-			Plugin.call($carousel, $carousel.data());
+			Plugin.call($(this), $(this).data());
 		});
 	});
+
 });
